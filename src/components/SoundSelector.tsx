ae29aa6fa3a6889452bb8a32a1492ee8b3eeddef
Quedow <182747452+Quedow@users.noreply.github.com>
2025-02-09 20:01:03 +0100

    feat: add new sound

--- src/components/SoundSelector.tsx
+++ src/components/SoundSelector.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import turnSound from '../assets/sounds/turnSound.mp3';
 import uwu from '../assets/sounds/uwu.mp3';
+import ohLaVAche from "../assets/sounds/ohLaVache.mp3";
 
 interface Props {
   setSound: (sound: string) => void;
@@ -14,6 +15,7 @@ export default function SoundSelector(props: Props) {
   const sounds: { [key: string]: string } = {
     turnSound: turnSound,
     uwu: uwu,
+    ohLaVAche: ohLaVAche,
   };
 
   const [sound, setSound] = useState<string>('turnSound');
